8cffff011359526f0558590573fab51a5150f12a
cainscreation <cainscreation@gmail.com>
2020-05-05 10:28:23 +0530

    Ajax

--- inc/dashboard.php
+++ inc/dashboard.php
@@ -9,7 +9,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#"><?php echo TALENTO; ?></a>
+            <a class="navbar-brand" href="#"><?php echo "TALENTO"; ?></a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
